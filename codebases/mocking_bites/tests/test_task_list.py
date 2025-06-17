987bcd3339f1abdfbbc8f102463ae7af61eb0d44
james lyddon <jameslyddon@gmail.com>
2025-06-17 17:00:21 +0100

    init

--- codebases/mocking_bites/tests/test_task_list.py
+++ codebases/mocking_bites/tests/test_task_list.py
@@ -1,5 +1,5 @@
 from lib.task_list import TaskList
-
+from unittest.mock import Mock
 
 def test_task_list_initially_empty():
     task_list = TaskList()
@@ -11,3 +11,26 @@ def test_tasks_initially_not_all_complete():
     assert task_list.all_complete() == False
 
 # Unit test `#tasks` and `#all_complete` behaviour
+def test_add_task():
+    task_list = TaskList()
+    task_1 = Mock()
+    task_2 = Mock()
+    task_list.add(task_1)
+    task_list.add(task_2)
+    assert task_list.all() == [task_1, task_2]
+
+def test_all_complete():
+    task_list = TaskList()
+    task_1 = Mock()
+    task_2 = Mock()
+    task_3 = Mock()
+    task_list.add(task_1)
+    task_list.add(task_2)
+    task_list.add(task_3)
+    task_1.is_complete.return_value = True
+    task_2.is_complete.return_value = False
+    task_3.is_complete.return_value = False
+    assert task_list.all_complete() == False
+    task_2.is_complete.return_value = True
+    task_3.is_complete.return_value = True
+    assert task_list.all_complete() == True
